7fd6d6aaab763bb5f0b5dfcf3f2e084f0c232eae
Nicolas Trudel <trudelnicolas94@gmail.com>
2020-06-07 23:08:15 -0400

    Fix responsive on text-box in hero-variant

--- wp-content/themes/tkarra/index.php
+++ wp-content/themes/tkarra/index.php
@@ -29,9 +29,8 @@
                 </div>
             </div>
 
-
-        <?php elseif (get_row_layout() == 'text_image'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+          <?php elseif (get_row_layout() == 'hero_variant'): ?>
+            <div id="<?php the_sub_field('id'); ?>" class="flex-row hero-variant" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
                 <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
                   <?php get_template_part('template-parts/content/content', 'text'); ?>
                 </div>
@@ -40,92 +39,103 @@
                 </div>
             </div>
 
-        <?php elseif (get_row_layout() == 'image_text'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="flex-row flex-row--reverse" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_image_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'image'); ?>
-                </div>
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'text'); ?>
-                </div>
-            </div>
 
-        <?php elseif (get_row_layout() == 'text_team'): ?>
+          <?php elseif (get_row_layout() == 'text_image'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'text'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_image_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'image'); ?>
+                  </div>
+              </div>
+
+          <?php elseif (get_row_layout() == 'image_text'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row flex-row--reverse" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_image_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'image'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'text'); ?>
+                  </div>
+              </div>
+
+          <?php elseif (get_row_layout() == 'text_team'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'text'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_image_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'team'); ?>
+                  </div>
+              </div>
+
+          <?php elseif (get_row_layout() == 'text_map'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'text'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_map_box'); ?>">
+                    <?php get_template_part('template-parts/map'); ?>
+                  </div>
+              </div>
+              
+          <?php elseif (get_row_layout() == 'map_text'): ?>
             <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'text'); ?>
-                </div>
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_image_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'team'); ?>
-                </div>
+              <div class="flex-col--12 flex-col--<?php the_sub_field('width_map_box'); ?>">
+                <?php get_template_part('template-parts/map'); ?>
+              </div>
+              <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                <?php get_template_part('template-parts/content/content', 'text'); ?>
+              </div>
             </div>
 
-        <?php elseif (get_row_layout() == 'text_map'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'text'); ?>
-                </div>
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_map_box'); ?>">
-                  <?php get_template_part('template-parts/map'); ?>
-                </div>
-            </div>
-            
-        <?php elseif (get_row_layout() == 'map_text'): ?>
-          <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
-            <div class="flex-col--12 flex-col--<?php the_sub_field('width_map_box'); ?>">
-              <?php get_template_part('template-parts/map'); ?>
-            </div>
-            <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
-              <?php get_template_part('template-parts/content/content', 'text'); ?>
-            </div>
-          </div>
+          <?php elseif (get_row_layout() == 'text_carrousel'): ?>
+              <div id="<?php the_sub_field('id'); ?>"  class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'text'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_carrousel_box'); ?>">
+                    <?php get_template_part('template-parts/carrousel/carrousel', 'arrow'); ?>
+                  </div>
+              </div>
 
-        <?php elseif (get_row_layout() == 'text_carrousel'): ?>
-            <div id="<?php the_sub_field('id'); ?>"  class="flex-row" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'text'); ?>
-                </div>
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_carrousel_box'); ?>">
-                  <?php get_template_part('template-parts/carrousel/carrousel', 'arrow'); ?>
-                </div>
-            </div>
+            <?php elseif (get_row_layout() == 'carrousel_text'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row flex-row--reverse" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                    <?php get_template_part('template-parts/content/content', 'text'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_carrousel_box'); ?>">
+                    <?php get_template_part('template-parts/carrousel/carrousel', 'arrow'); ?>
+                  </div>
+              </div>
 
-          <?php elseif (get_row_layout() == 'carrousel_text'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="flex-row flex-row--reverse" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
-                  <?php get_template_part('template-parts/content/content', 'text'); ?>
-                </div>
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_carrousel_box'); ?>">
-                  <?php get_template_part('template-parts/carrousel/carrousel', 'arrow'); ?>
-                </div>
-            </div>
+            <?php elseif (get_row_layout() == 'simple_text'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row">
+                  <div class="flex-col--12">
+                    <?php get_template_part('template-parts/content/content', 'simple'); ?>
+                  </div>
+              </div>
 
-          <?php elseif (get_row_layout() == 'simple_text'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="flex-row">
-                <div class="flex-col--12">
-                  <?php get_template_part('template-parts/content/content', 'simple'); ?>
-                </div>
-            </div>
+            <?php elseif (get_row_layout() == 'zone_team_presentation'):
+              get_template_part('template-parts/content/content', 'presentation');
+            ?>
 
-          <?php elseif (get_row_layout() == 'zone_team_presentation'):
-            get_template_part('template-parts/content/content', 'presentation');
-          ?>
+            <?php elseif (get_row_layout() === 'contact'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="color: <?php the_sub_field('text_color'); ?>;">
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_zone_contact'); ?>" style="background-color: <?php the_sub_field('color_zone_contact'); ?>">
+                    <?php get_template_part('template-parts/contact/contact', 'content'); ?>
+                  </div>
+                  <div class="flex-col--12 flex-col--<?php the_sub_field('width_zone_form'); ?>">
+                    <?php get_template_part('template-parts/contact/contact', 'form'); ?>
+                  </div>
+              </div>
 
-          <?php elseif (get_row_layout() === 'contact'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="flex-row" style="color: <?php the_sub_field('text_color'); ?>;">
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_zone_contact'); ?>" style="background-color: <?php the_sub_field('color_zone_contact'); ?>">
-                  <?php get_template_part('template-parts/contact/contact', 'content'); ?>
-                </div>
-                <div class="flex-col--12 flex-col--<?php the_sub_field('width_zone_form'); ?>">
-                  <?php get_template_part('template-parts/contact/contact', 'form'); ?>
-                </div>
-            </div>
-
-          <?php elseif (get_row_layout() === 'full_map'): ?>
-            <div id="<?php the_sub_field('id'); ?>" class="map-container">
-              <?php get_template_part('template-parts/map'); ?>
-            </div>
-          <?php endif; ?>
+            <?php elseif (get_row_layout() === 'full_map'): ?>
+              <div id="<?php the_sub_field('id'); ?>" class="map-container">
+                <?php get_template_part('template-parts/map'); ?>
+              </div>
+            <?php endif; ?>
         <?php endwhile;
 
       else :
--- wp-content/themes/tkarra/page.php
+++ wp-content/themes/tkarra/page.php
@@ -25,11 +25,21 @@ $nbRow = 0;
           the_row(); ?>
 
           <?php if (get_row_layout() == 'hero'): ?>
-          <div id="<?php the_sub_field('id'); ?>" class="hero flex-row" style="background-color: <?php the_sub_field('color');?>; color: <?php the_sub_field('text_color'); ?>;">
-            <div class="flex-col--12">
-              <?php get_template_part('template-parts/hero/hero', 'content'); ?>
+            <div id="<?php the_sub_field('id'); ?>" class="hero flex-row" style="background-color: <?php the_sub_field('color');?>; color: <?php the_sub_field('text_color'); ?>;">
+              <div class="flex-col--12">
+                <?php get_template_part('template-parts/hero/hero', 'content'); ?>
+              </div>
             </div>
-          </div>
+
+          <?php elseif (get_row_layout() == 'hero_variant'): ?>
+            <div id="<?php the_sub_field('id'); ?>" class="flex-row hero-variant" style="background-color: <?php the_sub_field('color'); ?>; color: <?php the_sub_field('text_color'); ?>;">
+                <div class="flex-col--12 flex-col--<?php the_sub_field('width_text_box'); ?>">
+                  <?php get_template_part('template-parts/content/content', 'text'); ?>
+                </div>
+                <div class="flex-col--12 flex-col--<?php the_sub_field('width_image_box'); ?>">
+                  <?php get_template_part('template-parts/content/content', 'image'); ?>
+                </div>
+            </div>
 
 
         <?php elseif (get_row_layout() == 'text_image'): ?>
